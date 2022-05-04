bb217e056f3550c0949deb620619f5b2eda1510a
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-05-04 13:13:21 +0200

    Added FPS

--- handTrackingMin.py
+++ handTrackingMin.py
@@ -2,8 +2,10 @@ import cv2 as cv
 import mediapipe as mp
 import time
 
-capture = cv.VideoCapture(0)
 
+capture = cv.VideoCapture(0)
+cTime = 0
+pTime = 0
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=4)
 mpDraw = mp.solutions.drawing_utils
@@ -16,5 +18,11 @@ while True:
         for handlms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img,handlms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img,str(int(fps)), (10,70), cv.FONT_HERSHEY_SIMPLEX,
+               3, (255,0,255),3)
     cv.imshow("Image", img)
     cv.waitKey(1)
