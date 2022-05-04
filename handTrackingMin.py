cbd7521bfe565b1b0b08522544d65fb2d98f4c08
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-05-04 12:25:22 +0200

    Hand Dots integerated

--- handTrackingMin.py
+++ handTrackingMin.py
@@ -1,11 +1,20 @@
 import cv2 as cv
-# import mediapipe as mp
+import mediapipe as mp
 import time
 
 capture = cv.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 while True:
     success, img = capture.read()
+    imgRGB = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handlms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img,handlms)
 
     cv.imshow("Image", img)
     cv.waitKey(1)
